242173347c0c2dc4f1601ed3f79376cdd12e5069
Subhadeep Barua <83444796+subhadeepbarua@users.noreply.github.com>
2023-12-12 22:25:27 +0530

    Update UserInputForm.jsx

--- src/components/UserInputForm.jsx
+++ src/components/UserInputForm.jsx
@@ -15,7 +15,7 @@ const UserInputForm = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000', {
+      const response = await fetch('https://booknode.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
