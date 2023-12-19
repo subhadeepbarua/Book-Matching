8f9b7aa9c9818f993ad1589e18efbfb0d73be1a6
Subhadeep Barua <83444796+subhadeepbarua@users.noreply.github.com>
2023-12-19 16:22:10 +0530

    Update UserInputForm.jsx

--- src/components/UserInputForm.jsx
+++ src/components/UserInputForm.jsx
@@ -24,7 +24,7 @@ const UserInputForm = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000', {
+      const response = await fetch('http://https://serverbook-qx5o.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
